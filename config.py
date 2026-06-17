ece3b42fa6aa802b2d19ab1d9512f99afccff3a0
PREM KUMAR MOKARA <premmokra143@gmail.com>
2026-06-17 17:35:25 +0530

    fix: align OpenAI Realtime session config with GA schema

--- config.py
+++ config.py
@@ -189,6 +189,14 @@ class Config:
                     },
                     'transcription': {
                         'model': 'whisper-1'
+                    },
+                    'turn_detection': {
+                        'type': 'server_vad',
+                        'threshold': 0.5,
+                        'prefix_padding_ms': 300,
+                        'silence_duration_ms': 200,
+                        'create_response': True,
+                        'interrupt_response': True
                     }
                 },
                 'output': {
@@ -198,7 +206,6 @@ class Config:
                     'voice': voice
                 }
             },
-            'turn_detection': {'type': 'server_vad', 'threshold': 0.5},
             'temperature': cls.TEMPERATURE,
-            'max_response_output_tokens': 4096
+            'max_output_tokens': 4096
         } 
